e872f0edcd82e0fa064f95d04e95ea1389e667d8
Ananta C <ananta.c@gmail.com>
2019-04-17 14:40:00 -0400

    added oracle os support

--- attributes/atomic-testing.rb
+++ attributes/atomic-testing.rb
@@ -8,7 +8,7 @@ case node['platform']
 when 'fedora'
   default['yum']['atomic-testing']['mirrorlist'] = 'https://updates.atomicorp.com/channels/mirrorlist/atomic-testing/fedora-$releasever-$basearch'
   default['yum']['atomic-testing']['description'] = 'Fedora Core $releasever - atomicrocketturtle.com - (Testing)'
-when 'redhat', 'centos', 'scientific'
+when 'redhat', 'centos', 'scientific', 'oracle'
   default['yum']['atomic-testing']['mirrorlist'] = 'https://updates.atomicorp.com/channels/mirrorlist/atomic-testing/centos-$releasever-$basearch'
   default['yum']['atomic-testing']['description'] = 'CentOS / Red Hat Enterprise Linux $releasever - atomicrocketturtle.com - (Testing)'
 end
--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,10 @@ maintainer_email 'brian.hays@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures yum-atomic repo'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '0.2.1'
 
 supports 'centos'
+supports 'oracle'
 supports 'redhat'
 supports 'scientific'
 supports 'fedora'
